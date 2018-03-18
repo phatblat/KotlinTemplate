119d351b5b66c0db2dcfe0b448f2dfc052808aa8
Ben Chatelain <ben@octop.ad>
2018-03-18 14:44:42 -0600

    💩 Extract version from plugins block for dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,19 @@
  * Spek-1.1.5
  */
 
-val junitPlatformVersion = "1.0.0"
-val spekVersion          = "1.1.5"
-
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.2.30"
     id("org.junit.platform.gradle.plugin") version "1.0.0"
+}
+
+val spekVersion          = "1.1.5"
+
+// This is necessary to make the plugin version accessible in other places
+// https://stackoverflow.com/questions/46053522/how-to-get-ext-variables-into-plugins-block-in-build-gradle-kts/47507441#47507441
+val junitPlatformVersion: String? by extra {
+    buildscript.configurations["classpath"]
+            .resolvedConfiguration.firstLevelModuleDependencies
+            .find { it.moduleName == "junit-platform-gradle-plugin" }?.moduleVersion
 }
 
 repositories {
@@ -16,6 +23,7 @@ repositories {
 }
 
 dependencies {
+    // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
