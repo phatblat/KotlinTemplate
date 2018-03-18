adc66044dfd0c73a56d4699a67c49feebbcd5f4a
Ben Chatelain <ben@octop.ad>
2018-03-17 20:58:34 -0600

    🎨 Clean up settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,10 @@
 rootProject.name = "Spek-1.1.5"
 
 // Workaround to make the JUnit Platform Gradle Plugin available using the `plugins` DSL
-    // https://github.com/junit-team/junit5/issues/768#issuecomment-330078905
+// See: https://github.com/junit-team/junit5/issues/768#issuecomment-330078905
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        // jcenter()
-        // maven { url = uri("") }
     }
     resolutionStrategy {
         eachPlugin {
